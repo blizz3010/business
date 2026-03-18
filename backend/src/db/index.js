6f3075882d064cfff98c68b4fc5b26c5e98518c8
blizz3010 <onomic23@gmail.com>
2026-03-18 11:58:30 -0400

    Refactor app to PostgreSQL-driven BI dashboard

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -7,7 +7,8 @@ dotenv.config();
 const { Pool } = pg;
 
 export const pgPool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 export const redis = new Redis(process.env.REDIS_URL);
